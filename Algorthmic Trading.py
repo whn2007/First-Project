439cfb6a4a13762bb59962204cb0a9195cce30ae
whn2007 <whn2007@nyu.edu>
2021-04-06 14:04:27 -0400

    Name Change
    Name Change

---
+++ Algorthmic Trading.py
@@ -0,0 +1,13 @@
+
+def main():
+    #3a.
+    print([10 ** i for i in range(6)])
+    #3b.
+    print( i*(i+1) for i in range(10))
+    #3c.
+    print([chr(i) for i in range(97,123)])
+
+
+if __name__ == '__main__':
+    main()
+
