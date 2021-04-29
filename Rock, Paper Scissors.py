0dd7db5c2054e43a74e201d1eaec62689c428fb1
whn2007 <whn2007@nyu.edu>
2021-04-29 16:19:36 -0400

    Update Rock, Paper Scissors.py

--- Rock, Paper Scissors.py
+++ Rock, Paper Scissors.py
@@ -11,7 +11,27 @@ def game():
 
     if player_input == "r" and computer_choice == "s":
         print("You win!")
-        input("Play again? (y)es/(n)o")
+    elif player_input == "r" and computer_choice == "p":
+        print("You lose!")
+    elif player_input == "s" and computer_choice == "p":
+        print("You win!")
+    elif player_input == "s" and computer_choice == "r":
+        print("You lose!")
+    elif player_input == "p" and computer_choice == "r":
+        print("You win!")
+    elif player_input == "p" and computer_choice == "s":
+        print("You lose!")
+    else:
+        print("Draw!")
+
+    play_again = input("Play again? (y)es/(n)o")
+    while play_again not in "yn":
+        play_again = input("Play again? (y)es/(n)o")
+    if play_again == "y":
+        game()
+    else:
+        print("Goodbye.")
+
 
 print("Ready to play?")
 game()
